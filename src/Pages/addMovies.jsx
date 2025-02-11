754afa8fc83a4678ae24e49a5a034a470656874b
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-11 21:33:01 +0100

    Fix: eseguite correzioni generiche su addMovie

--- src/Pages/addMovies.jsx
+++ src/Pages/addMovies.jsx
@@ -10,7 +10,7 @@ const AddMovie = () =>{
   const initialData = {
     title: '',
     genre: '',
-    abstract: '',
+    director: '',
     image: null
   }
 
@@ -34,7 +34,7 @@ const AddMovie = () =>{
       dataToSend.append(key, formData[key])
     }
 
-    axios.post(api_url, dataToSend, {headers: {'content-Type': 'multipart/formData'}})
+    axios.post(api_url, dataToSend, {headers: { 'Content-Type': 'multipart/form-data' } })
     .then(res => {
       console.log(res.data);
       setFormData(initialData)
@@ -50,29 +50,29 @@ const AddMovie = () =>{
   return (  
     <div>
       <div>
-        <h1>Aggiungi la tua recensione</h1>
+        <h1>Aggiungi un nuovo Film</h1>
       </div>
       <form action="#" onSubmit={handleSubmit}>
         <p className="text-danger">{errorMsg}</p>
           <div className="card">
             <div className="input-group mb-3 mt-3">
-              <span className="input-group-text" id="inputGroup-sizing-default">Nome</span>
-              <input type="text" name="name" value={formData.name} onChange={handleValue} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+              <span className="input-group-text" id="inputGroup-sizing-default">Titolo</span>
+              <input type="text" name="title" value={formData.title} onChange={handleValue} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
             </div>
 
             <div className="input-group">
-              <span className="input-group-text">Scrivi la tua recensione</span>
-              <textarea type="text" name="text" value={formData.text} onChange={handleValue} className="form-control" aria-label="With textarea"></textarea>
+              <span className="input-group-text">Genere</span>
+              <input type="text" name="genre" value={formData.genre} onChange={handleValue} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
             </div>
 
             <div className="input-group mb-3 mt-3">
-              <span className="input-group-text" id="inputGroup-sizing-default">Voto</span>
-              <input type="number" name="vote" value={formData.vote} onChange={handleValue} min={1} max={5} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+              <span className="input-group-text" id="inputGroup-sizing-default">Regista</span>
+              <input type="text" name="director" value={formData.director} onChange={handleValue} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
             </div>
 
-            <div className="mb-3">
-              <label for="formFile" className="form-label">Default file input example</label>
-              <input className="form-control" type="file" id="formFile" />
+            <div className="input-group mb-3 mt-3">
+              <span className="input-group-text" id="inputGroup-sizing-default">Immagine</span>
+              <input type="file" name="image" onChange={handleValue} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
             </div>
           </div>
           <div>
